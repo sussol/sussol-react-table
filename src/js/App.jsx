ca65d76cbeb8c641c40fece54884c0e056fefcf3
Christopher Petty <christopher.m.petty@gmail.com>
2017-02-02 15:15:00 +1300

    Table part way through pulling into a wrapper

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,21 +1,116 @@
 import React from 'react';
-import { Cell, Column, Table } from '@blueprintjs/table';
-// import '@blueprintjs/table/dist/table.css';
-// import '../css/normalize.css';
-// import '../../node_modules/@blueprintjs/core/dist/blueprint.css';
+import { ColumnHeaderCell, EditableCell, Column, Table, TruncatedFormat } from '@blueprintjs/table';
+import FlatButton from 'material-ui/FlatButton';
+import FontIcon from 'material-ui/FontIcon';
+
+class SussolReactTable extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      isAscending: true,
+    };
+    this.renderCell = this.renderCell.bind(this);
+    this.renderColumnHeader = this.renderColumnHeader.bind(this);
+    this.toggleSortOrder = this.toggleSortOrder.bind(this);
+    this.renderSortButton = this.renderSortButton.bind(this);
+  }
+
+  toggleSortOrder() {
+    this.setState({
+      isAscending: !this.state.isAscending,
+    });
+  }
+
+  renderSortButton() {
+    return <p onClick={this.toggleSortOrder}>Toggle sort order</p>;
+  }
+
+  renderColumnHeader() {
+    return (
+      <ColumnHeaderCell >
+        <FlatButton
+          label="Dollars"
+          labelPosition="before"
+          icon={<FontIcon className="material-icons" >{this.state.isAscending ? 'arrow_drop_up' : 'arrow_drop_down'}</FontIcon>}
+          onClick={this.toggleSortOrder}
+          style={{ width: '100%' }}
+        />
+      </ColumnHeaderCell>
+    );
+  }
+
+  renderCell(rowIndex) {
+    return (
+      <EditableCell
+        truncated={false}
+        onConfirm={(newValue)=>{ console.log(`Changed to ${newValue}`) }}
+        value={
+          rowIndex % 3 === 0 ? 'A really really really really really really really really really long string!!' : `${this.state.isAscending}`
+        }
+      >
+      </EditableCell>
+    );
+  }
+
+  render() {
+    return (
+      <div style={{height:400}} >
+        <Table numRows={100000}>
+            <Column renderCell={this.renderCell} renderColumnHeader={this.renderColumnHeader} />
+        </Table>
+      </div>
+    );
+  }
+}
 
 
-export function App(props) {
-  const renderCell = (rowIndex: number) => {
-    return <Cell>{`$${(rowIndex * 10).toFixed(2)}`}</Cell>;
-  };
 
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <Table numRows={300}>
-          <Column name="Dollars" renderCell={renderCell} />
-      </Table>
-    </div>
-  );
+export class App extends React.Component {
+  constructor(...args) {
+    super(...args);
+    this.state = {
+      isAscending: true,
+    };
+    this.renderCell = this.renderCell.bind(this);
+    this.renderColumnHeader = this.renderColumnHeader.bind(this);
+    this.toggleSortOrder = this.toggleSortOrder.bind(this);
+    this.renderSortButton = this.renderSortButton.bind(this);
+  }
+
+  toggleSortOrder() {
+    this.setState({
+      isAscending: !this.state.isAscending,
+    });
+  }
+
+  renderSortButton() {
+    return <p onClick={this.toggleSortOrder}>Toggle sort order</p>;
+  }
+
+  renderColumnHeader() {
+    return (
+      <ColumnHeaderCell >
+        <FlatButton
+          label="Dollars"
+          labelPosition="before"
+          icon={<FontIcon className="material-icons" >{this.state.isAscending ? 'arrow_drop_up' : 'arrow_drop_down'}</FontIcon>}
+          onClick={this.toggleSortOrder}
+          style={{ width: '100%' }}
+        />
+      </ColumnHeaderCell>
+    );
+  }
+
+  renderCell(rowIndex) {
+    return <EditableCell truncated={false} onConfirm={(newValue) => {console.log(`Changed to ${newValue}`);}} value={rowIndex % 3 === 0 ? 'A really really really really really really really really really long string!!' : `${this.state.isAscending}`}></EditableCell>;
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <SussolReactTable />
+      </div>
+    );
+  }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { App } from './App.jsx'
+import { App } from './App.jsx';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 // Needed for onTouchTap, won't be required once official React version released.
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-render(<App />, document.getElementById('root'));
+const muiTheme = getMuiTheme({
+  button: {
+    textTransform: 'none',
+  },
+});
+
+const StyledApp = () => (
+  <MuiThemeProvider muiTheme={muiTheme}>
+    <App />
+  </MuiThemeProvider>
+);
+
+render(<StyledApp />, document.getElementById('root'));
