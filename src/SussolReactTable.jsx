ae98ec5210619799dded29c755453009db94557e
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-08-18 18:35:15 +0545

    [basic-sort] sorting added; package updates, cleanup

--- src/SussolReactTable.jsx
+++ src/SussolReactTable.jsx
@@ -2,8 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Cell, ColumnHeaderCell, EditableCell, Column, Table } from '@blueprintjs/table';
 import FlatButton from 'material-ui/FlatButton';
-import FontIcon from 'material-ui/FontIcon';
 
+/**
+* compare
+*
+* Provides a basic implementation of either a sort ASC or sort DESC comparator.
+*
+* @param {any}     data which can be converted to a string
+* @param {any}     data which can be converted to a string
+* @param {bool}    isAscending: true/false
+* @return {int}    e.g. -1, 0, 1
+*/
+const compare = (a, b, isAscending) => {
+  return isAscending
+    ? a.toString().localeCompare(b)
+    : b.toString().localeCompare(a);
+}
+
+const noop = () => {};
 
 export class SussolReactTable extends React.Component {
   constructor(props, ...args) {
@@ -19,6 +35,7 @@ export class SussolReactTable extends React.Component {
     this.renderCell = this.renderCell.bind(this);
     this.renderColumnHeader = this.renderColumnHeader.bind(this);
     this.renderEditableCell = this.renderEditableCell.bind(this);
+    this.sortColumn = this.sortColumn.bind(this);
     this.toggleSortOrder = this.toggleSortOrder.bind(this);
     this.renderColumns = this.renderColumns.bind(this);
   }
@@ -31,6 +48,27 @@ export class SussolReactTable extends React.Component {
     this.setState({
       sortBy: columnKey,
       isAscending: !this.state.isAscending,
+      tableData: this.sortColumn(columnKey, compare),
+    });
+  }
+
+  /**
+  * sortColumn
+  *
+  * Provides sorted data: ASC or DESC given a column key and a comparator func.
+  *
+  * @param {str}      columnKey: column key string
+  * @param {func}     comparator: sort func
+  * @return {arr}     sorted array
+  */
+  sortColumn(columnKey, comparator) {
+    const { tableData } = this.state;
+    return tableData.sort((a, b) => {
+      return comparator(
+        a[columnKey],
+        b[columnKey],
+        this.state.isAscending,
+      );
     });
   }
 
@@ -42,24 +80,27 @@ export class SussolReactTable extends React.Component {
     });
   }
 
-// Renders column headers. Gets the sort direction from state and the label columns array.
+  // Renders column headers. Gets the sort direction from state and the label columns array.
   renderColumnHeader(column) {
     let sortIcon;
 
-    if ( column.sortable &&this.state.sortBy === column.key ) {
-      sortIcon = (
-        <FontIcon className="material-icons">
-          {this.state.isAscending ? 'arrow_drop_up' : 'arrow_drop_down'}
-        </FontIcon>
-      );
+    if (column.sortable && this.state.sortBy === column.key) {
+      sortIcon = this.state.isAscending
+        ? (this.renderSortIcon('asc'))
+        : (this.renderSortIcon('desc'));
     }
+
     return (
-      <ColumnHeaderCell >
+      <ColumnHeaderCell>
         <FlatButton
           label={column.title}
           labelPosition="before"
           icon={sortIcon}
-          onClick={() => this.toggleSortOrder(column.key)}
+          onClick={
+            column.sortable
+            ? () => this.toggleSortOrder(column.key)
+            : noop
+          }
           style={{ width: '100%' }}
         />
       </ColumnHeaderCell>
@@ -106,6 +147,12 @@ export class SussolReactTable extends React.Component {
       );
     });
     return columns;
+  }
+
+  renderSortIcon(direction) {
+    return direction === 'asc'
+      ? <svg width="18" height="18" viewBox="0 0 24 24"><path d="M7 10l5 5 5-5z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg>
+      : <svg width="18" height="18" viewBox="0 0 24 24"><path d="M7 14l5-5 5 5z"></path><path d="M0 0h24v24H0z" fill="none"></path></svg>;
   }
 
   render() {
