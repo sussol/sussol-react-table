7eecf04906b65ab3fa19a91962f9218e5e1a324b
Jeremiah Trein <jeremiahtrein@gmail.com>
2017-10-10 19:23:30 +0545

    [basic-sort] basic tests added

--- src/__tests__/SussolReactTable.spec.js
+++ src/__tests__/SussolReactTable.spec.js
@@ -1,1 +1,33 @@
-// Wow such coverage
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import { SussolReactTable } from '../';
+
+test('renders Blueprint.js table', () => {
+  const table = shallow(
+    <SussolReactTable
+      columns={[]}
+      tableData={[]}
+    />
+  )
+
+  expect(table.find('Table').length).toBe(1);
+});
+
+test('renders Blueprint.js table with props', () => {
+  const table = shallow(
+    <SussolReactTable
+      columns={[{
+        key: 'name',
+        title: 'Name',
+      }, {
+        key: 'code',
+        title: 'Code',
+      }]}
+      tableData={[{ name: 'hi', title: 'Hi!'}]}
+    />
+  )
+
+  expect(table.props().tableData.length).toBe(1);
+  expect(table.props().columns.length).toBe(2);
+});
